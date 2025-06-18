66f7329b2652644734800f4bc2f3f429455873dd
Elias Navarro <eliasnvr@icloud.com>
2025-06-18 20:11:58 -0300

    primer commit

---
+++ src/main/java/com/example/demo/ArtecheOjedaNavarroZandonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtecheOjedaNavarroZandonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtecheOjedaNavarroZandonaApplication.class, args);
+	}
+
+}
