72fea24cc261256ead5ff8c326bfe213d3142d22
Elias Navarro <eliasnvr@icloud.com>
2025-06-20 18:22:28 -0300

    agregado package controladores

--- src/main/java/servicios/RecetaService.java
+++ src/main/java/servicios/RecetaService.java
@@ -1,4 +1,4 @@
-package services;
+package servicios;
 
 public interface RecetaService {
 
--- src/main/java/servicios/RecetaServiceImpl.java
+++ src/main/java/servicios/RecetaServiceImpl.java
@@ -1,4 +1,4 @@
-package services;
+package servicios;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
